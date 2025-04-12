aa54cb2efcb54194cf136dd64b8437189728b61d
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-04-12 12:46:51 +0300

    Update q_shared.h

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define	STANDALONE				1
 
-#define Q3_VERSION            "SourceTech 1.4"
+#define Q3_VERSION            "SourceTech 1.5"
 #ifndef SVN_VERSION
   #define SVN_VERSION Q3_VERSION
 #endif
