10b30554f74ca69f17ccf73e1a5b33ec40526d0b
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-04-20 21:52:38 +0300

    Update sv_bot.c

--- code/server/sv_bot.c
+++ code/server/sv_bot.c
@@ -49,13 +49,13 @@ int SV_BotAllocateClient( void ) {
 	client_t	*cl;
 
 	// find a client slot
-	for ( i = 0, cl = svs.clients; i < sv.maxclients-1; i++, cl++ ) {
+	for ( i = 0, cl = svs.clients; i < sv.maxclients; i++, cl++ ) {
 		if ( cl->state == CS_FREE ) {
 			break;
 		}
 	}
 
-	if ( i == sv.maxclients-1 ) {
+	if ( i == sv.maxclients ) {
 		return -1;
 	}
 
