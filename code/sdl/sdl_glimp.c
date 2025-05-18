196cbfd9ae50fd61ad05d208095fa4706fb1efbf
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-05-18 17:30:46 +0300

    Update sdl_glimp.c

--- code/sdl/sdl_glimp.c
+++ code/sdl/sdl_glimp.c
@@ -57,8 +57,7 @@ cvar_t *in_nograb;
 GLimp_Shutdown
 ===============
 */
-void GLimp_Shutdown( qboolean unloadDLL )
-{
+void GLimp_Shutdown( qboolean unloadDLL ) {
 	IN_Shutdown();
 
 	SDL_DestroyWindow( SDL_window );
@@ -71,31 +70,7 @@ void GLimp_Shutdown( qboolean unloadDLL )
 		SDL_QuitSubSystem( SDL_INIT_VIDEO );
 }
 
-/*
-===============
-GLimp_Minimize
-
-Minimize the game so that user is back at the desktop
-===============
-*/
-void GLimp_Minimize( void )
-{
-	SDL_MinimizeWindow( SDL_window );
-}
-
-
-/*
-===============
-GLimp_LogComment
-===============
-*/
-void GLimp_LogComment( const char *comment )
-{
-}
-
-
-static int FindNearestDisplay( int *x, int *y, int w, int h )
-{
+static int FindNearestDisplay( int *x, int *y, int w, int h ) {
 	const int cx = *x + w / 2;
 	const int cy = *y + h / 2;
 	int i, index, numDisplays;
@@ -111,15 +86,12 @@ static int FindNearestDisplay( int *x, int *y, int w, int h )
 
 	list = Z_Malloc( numDisplays * sizeof( list[0] ) );
 
-	for ( i = 0; i < numDisplays; i++ )
-	{
+	for ( i = 0; i < numDisplays; i++ ) {
 		SDL_GetDisplayBounds( i, list + i );
-		//Com_Printf( "[%i]: x=%i, y=%i, w=%i, h=%i\n", i, list[i].x, list[i].y, list[i].w, list[i].h );
 	}
 
 	// select display by window center intersection
-	for ( i = 0; i < numDisplays; i++ )
-	{
+	for ( i = 0; i < numDisplays; i++ ) {
 		m = list + i;
 		if ( cx >= m->x && cx < (m->x + m->w) && cy >= m->y && cy < (m->y + m->h) )
 		{
@@ -129,13 +101,11 @@ static int FindNearestDisplay( int *x, int *y, int w, int h )
 	}
 
 	// select display by nearest distance between window center and display center
-	if ( index == -1 )
-	{
+	if ( index == -1 ) {
 		unsigned long nearest, dist;
 		int dx, dy;
 		nearest = ~0UL;
-		for ( i = 0; i < numDisplays; i++ )
-		{
+		for ( i = 0; i < numDisplays; i++ ) {
 			m = list + i;
 			dx = (m->x + m->w/2) - cx;
 			dy = (m->y + m->h/2) - cy;
@@ -149,8 +119,7 @@ static int FindNearestDisplay( int *x, int *y, int w, int h )
 	}
 
 	// adjust x and y coordinates if needed
-	if ( index >= 0 )
-	{
+	if ( index >= 0 ) {
 		m = list + index;
 		if ( *x < m->x )
 			*x = m->x;
@@ -164,23 +133,19 @@ static int FindNearestDisplay( int *x, int *y, int w, int h )
 	return index;
 }
 
-
-static SDL_HitTestResult SDL_HitTestFunc( SDL_Window *win, const SDL_Point *area, void *data )
-{
+static SDL_HitTestResult SDL_HitTestFunc( SDL_Window *win, const SDL_Point *area, void *data ) {
 	if ( Key_GetCatcher() & KEYCATCH_CONSOLE && keys[ K_ALT ].down )
 		return SDL_HITTEST_DRAGGABLE;
 
 	return SDL_HITTEST_NORMAL;
 }
 
-
 /*
 ===============
 GLimp_SetMode
 ===============
 */
-static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qboolean vulkan )
-{
+static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen ) {
 	glconfig_t *config = glw_state.config;
 	int perChannelColorBits;
 	int colorBits, depthBits, stencilBits;
@@ -192,44 +157,33 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 	Uint32 flags = SDL_WINDOW_SHOWN;
 
 #ifdef USE_VULKAN_API
-	if ( vulkan ) {
-		flags |= SDL_WINDOW_VULKAN;
-		Com_Printf( "Initializing Vulkan display\n");
-	} else
+	flags |= SDL_WINDOW_VULKAN;
+	Com_Printf( "Initializing Vulkan display\n");
 #endif
-	{
-		flags |= SDL_WINDOW_OPENGL;
-		Com_Printf( "Initializing OpenGL display\n");
-	}
+#ifdef USE_OPENGL_API
+	flags |= SDL_WINDOW_OPENGL;
+	Com_Printf( "Initializing OpenGL display\n");
+#endif
 
 	// If a window exists, note its display index
-	if ( SDL_window != NULL )
-	{
+	if ( SDL_window != NULL ) {
 		display = SDL_GetWindowDisplayIndex( SDL_window );
-		if ( display < 0 )
-		{
+		if ( display < 0 ) {
 			Com_DPrintf( "SDL_GetWindowDisplayIndex() failed: %s\n", SDL_GetError() );
 		}
-	}
-	else
-	{
+	} else {
 		x = vid_xpos->integer;
 		y = vid_ypos->integer;
 
 		// find out to which display our window belongs to
 		// according to previously stored \vid_xpos and \vid_ypos coordinates
 		display = FindNearestDisplay( &x, &y, 640, 480 );
-
-		//Com_Printf("Selected display: %i\n", display );
 	}
 
-	if ( display >= 0 && SDL_GetDesktopDisplayMode( display, &desktopMode ) == 0 )
-	{
+	if ( display >= 0 && SDL_GetDesktopDisplayMode( display, &desktopMode ) == 0 ) {
 		glw_state.desktop_width = desktopMode.w;
 		glw_state.desktop_height = desktopMode.h;
-	}
-	else
-	{
+	} else {
 		glw_state.desktop_width = 640;
 		glw_state.desktop_height = 480;
 	}
@@ -239,8 +193,7 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 
 	Com_Printf( "...setting mode %d:", mode );
 
-	if ( !CL_GetModeInfo( &config->vidWidth, &config->vidHeight, &config->windowAspect, mode, modeFS, glw_state.desktop_width, glw_state.desktop_height, fullscreen ) )
-	{
+	if ( !CL_GetModeInfo( &config->vidWidth, &config->vidHeight, &config->windowAspect, mode, modeFS, glw_state.desktop_width, glw_state.desktop_height, fullscreen ) ) {
 		Com_Printf( " invalid mode\n" );
 		return RSERR_INVALID_MODE;
 	}
@@ -248,14 +201,12 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 	Com_Printf( " %d %d\n", config->vidWidth, config->vidHeight );
 
 	// Destroy existing state if it exists
-	if ( SDL_glContext != NULL )
-	{
+	if ( SDL_glContext != NULL ) {
 		SDL_GL_DeleteContext( SDL_glContext );
 		SDL_glContext = NULL;
 	}
 
-	if ( SDL_window != NULL )
-	{
+	if ( SDL_window != NULL ) {
 		SDL_GetWindowPosition( SDL_window, &x, &y );
 		Com_DPrintf( "Existing window at %dx%d before being destroyed\n", x, y );
 		SDL_DestroyWindow( SDL_window );
@@ -265,36 +216,31 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 	gw_active = qfalse;
 	gw_minimized = qtrue;
 
-	if ( fullscreen )
-	{
+	if ( fullscreen ) {
 #ifdef MACOS_X
 		flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
 #else
 		flags |= SDL_WINDOW_FULLSCREEN;
 #endif
 	}
-	else if ( r_noborder->integer )
-	{
+	else if ( r_noborder->integer ) {
 		flags |= SDL_WINDOW_BORDERLESS;
 	}
-
-	//flags |= SDL_WINDOW_ALLOW_HIGHDPI;
 
 	colorBits = r_colorbits->value;
 
 	if ( colorBits == 0 || colorBits > 24 )
 		colorBits = 24;
 
-	if ( cl_depthbits->integer == 0 )
-	{
+	if ( cl_depthbits->integer == 0 ) {
 		// implicitly assume Z-buffer depth == desktop color depth
 		if ( colorBits > 16 )
 			depthBits = 24;
 		else
 			depthBits = 16;
-	}
-	else
+	} else {
 		depthBits = cl_depthbits->integer;
+	}
 
 	stencilBits = cl_stencilbits->integer;
 
@@ -302,8 +248,7 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 	if ( depthBits < 24 )
 		stencilBits = 0;
 
-	for ( i = 0; i < 16; i++ )
-	{
+	for ( i = 0; i < 16; i++ ) {
 		int testColorBits, testDepthBits, testStencilBits;
 		int realColorBits[3];
 
@@ -311,11 +256,9 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 		// 1 - minus colorBits
 		// 2 - minus depthBits
 		// 3 - minus stencil
-		if ((i % 4) == 0 && i)
-		{
+		if ((i % 4) == 0 && i) {
 			// one pass, reduce
-			switch (i / 4)
-			{
+			switch (i / 4) {
 				case 2 :
 					if (colorBits == 24)
 						colorBits = 16;
@@ -337,20 +280,17 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 		testDepthBits = depthBits;
 		testStencilBits = stencilBits;
 
-		if ((i % 4) == 3)
-		{ // reduce colorBits
+		if ((i % 4) == 3) { // reduce colorBits
 			if (testColorBits == 24)
 				testColorBits = 16;
 		}
 
-		if ((i % 4) == 2)
-		{ // reduce depthBits
+		if ((i % 4) == 2) { // reduce depthBits
 			if (testDepthBits == 24)
 				testDepthBits = 16;
 		}
 
-		if ((i % 4) == 1)
-		{ // reduce stencilBits
+		if ((i % 4) == 1) { // reduce stencilBits
 			if (testStencilBits == 8)
 				testStencilBits = 0;
 		}
@@ -360,48 +300,33 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 		else
 			perChannelColorBits = 4;
 
-#ifdef USE_VULKAN_API
-		if ( !vulkan )
-#endif
-		{
-	
-#ifdef __sgi /* Fix for SGIs grabbing too many bits of color */
-			if (perChannelColorBits == 4)
-				perChannelColorBits = 0; /* Use minimum size for 16-bit color */
+#ifndef USE_VULKAN_API
+		SDL_GL_SetAttribute( SDL_GL_RED_SIZE, perChannelColorBits );
+		SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, perChannelColorBits );
+		SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, perChannelColorBits );
+		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, testDepthBits );
+		SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, testStencilBits );
 
-			/* Need alpha or else SGIs choose 36+ bit RGB mode */
-			SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 1 );
-#endif
+		SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 0 );
+		SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 0 );
 
-			SDL_GL_SetAttribute( SDL_GL_RED_SIZE, perChannelColorBits );
-			SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, perChannelColorBits );
-			SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, perChannelColorBits );
-			SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, testDepthBits );
-			SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, testStencilBits );
-
-			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 0 );
-			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 0 );
-
-			SDL_GL_SetAttribute( SDL_GL_STEREO, 0 );
+		SDL_GL_SetAttribute( SDL_GL_STEREO, 0 );
 		
-			SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+		SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
-			if ( !r_allowSoftwareGL->integer )
-				SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
-		}
+		if ( !r_allowSoftwareGL->integer )
+			SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
+#endif
 
-		if ( ( SDL_window = SDL_CreateWindow( cl_title, x, y, config->vidWidth, config->vidHeight, flags ) ) == NULL )
-		{
+		if ( ( SDL_window = SDL_CreateWindow( cl_title, x, y, config->vidWidth, config->vidHeight, flags ) ) == NULL ) {
 			Com_DPrintf( "SDL_CreateWindow failed: %s\n", SDL_GetError() );
 			continue;
 		}
 
-		if ( fullscreen )
-		{
+		if ( fullscreen ) {
 			SDL_DisplayMode mode;
 
-			switch ( testColorBits )
-			{
+			switch ( testColorBits ) {
 				case 16: mode.format = SDL_PIXELFORMAT_RGB565; break;
 				case 24: mode.format = SDL_PIXELFORMAT_RGB24;  break;
 				default: Com_DPrintf( "testColorBits is %d, can't fullscreen\n", testColorBits ); continue;
@@ -412,14 +337,12 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 			mode.refresh_rate = /* config->displayFrequency = */ Cvar_VariableIntegerValue( "r_displayRefresh" );
 			mode.driverdata = NULL;
 
-			if ( SDL_SetWindowDisplayMode( SDL_window, &mode ) < 0 )
-			{
+			if ( SDL_SetWindowDisplayMode( SDL_window, &mode ) < 0 ) {
 				Com_DPrintf( "SDL_SetWindowDisplayMode failed: %s\n", SDL_GetError( ) );
 				continue;
 			}
 
-			if ( SDL_GetWindowDisplayMode( SDL_window, &mode ) >= 0 )
-			{
+			if ( SDL_GetWindowDisplayMode( SDL_window, &mode ) >= 0 ) {
 				config->displayFrequency = mode.refresh_rate;
 				config->vidWidth = mode.w;
 				config->vidHeight = mode.h;
@@ -427,70 +350,38 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 		}
 
 #ifdef USE_VULKAN_API
-		if ( vulkan )
-		{
-			config->colorBits = testColorBits;
-			config->depthBits = testDepthBits;
-			config->stencilBits = testStencilBits;
-		}
-		else
+		config->colorBits = testColorBits;
+		config->depthBits = testDepthBits;
+		config->stencilBits = testStencilBits;
 #endif
-		{
-			if ( !SDL_glContext )
-			{
-				if ( ( SDL_glContext = SDL_GL_CreateContext( SDL_window ) ) == NULL )
-				{
-					Com_DPrintf( "SDL_GL_CreateContext failed: %s\n", SDL_GetError( ) );
-					SDL_DestroyWindow( SDL_window );
-					SDL_window = NULL;
-					continue;
-				}
+#ifdef USE_OPENGL_API
+		if ( !SDL_glContext ) {
+			if ( ( SDL_glContext = SDL_GL_CreateContext( SDL_window ) ) == NULL ) {
+				Com_DPrintf( "SDL_GL_CreateContext failed: %s\n", SDL_GetError( ) );
+				SDL_DestroyWindow( SDL_window );
+				SDL_window = NULL;
+				continue;
 			}
+		}
 
-			if ( SDL_GL_SetSwapInterval( r_swapInterval->integer ) == -1 )
-			{
-				Com_DPrintf( "SDL_GL_SetSwapInterval failed: %s\n", SDL_GetError( ) );
-			}
+		if ( SDL_GL_SetSwapInterval( r_swapInterval->integer ) == -1 ) {
+			Com_DPrintf( "SDL_GL_SetSwapInterval failed: %s\n", SDL_GetError( ) );
+		}
 
-			SDL_GL_GetAttribute( SDL_GL_RED_SIZE, &realColorBits[0] );
-			SDL_GL_GetAttribute( SDL_GL_GREEN_SIZE, &realColorBits[1] );
-			SDL_GL_GetAttribute( SDL_GL_BLUE_SIZE, &realColorBits[2] );
-			SDL_GL_GetAttribute( SDL_GL_DEPTH_SIZE, &config->depthBits );
-			SDL_GL_GetAttribute( SDL_GL_STENCIL_SIZE, &config->stencilBits );
+		SDL_GL_GetAttribute( SDL_GL_RED_SIZE, &realColorBits[0] );
+		SDL_GL_GetAttribute( SDL_GL_GREEN_SIZE, &realColorBits[1] );
+		SDL_GL_GetAttribute( SDL_GL_BLUE_SIZE, &realColorBits[2] );
+		SDL_GL_GetAttribute( SDL_GL_DEPTH_SIZE, &config->depthBits );
+		SDL_GL_GetAttribute( SDL_GL_STENCIL_SIZE, &config->stencilBits );
 
-			config->colorBits = realColorBits[0] + realColorBits[1] + realColorBits[2];
-		} // if ( !vulkan )
-
+		config->colorBits = realColorBits[0] + realColorBits[1] + realColorBits[2];
+#endif
 
 		Com_Printf( "Using %d color bits, %d depth, %d stencil display.\n",	config->colorBits, config->depthBits, config->stencilBits );
-
 		break;
 	}
 
-	if ( SDL_window )
-	{
-/*#ifdef USE_ICON
-		SDL_Surface *icon = SDL_CreateRGBSurfaceFrom(
-			(void *)CLIENT_WINDOW_ICON.pixel_data,
-			CLIENT_WINDOW_ICON.width,
-			CLIENT_WINDOW_ICON.height,
-			CLIENT_WINDOW_ICON.bytes_per_pixel * 8,
-			CLIENT_WINDOW_ICON.bytes_per_pixel * CLIENT_WINDOW_ICON.width,
-#ifdef Q3_LITTLE_ENDIAN
-			0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000
-#else
-			0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF
-#endif
-		);
-		if ( icon )
-		{
-			SDL_SetWindowIcon( SDL_window, icon );
-			SDL_FreeSurface( icon );
-		}
-#endif*/
-	}
-	else
-	{
+	if ( !SDL_window ) {
 		Com_Printf( "Couldn't get a visual\n" );
 		return RSERR_INVALID_MODE;
 	}
@@ -499,11 +390,11 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 		SDL_SetWindowHitTest( SDL_window, SDL_HitTestFunc, NULL );
 
 #ifdef USE_VULKAN_API
-	if ( vulkan )
 		SDL_Vulkan_GetDrawableSize( SDL_window, &config->vidWidth, &config->vidHeight );
-	else
 #endif
+#ifdef USE_OPENGL_API
 		SDL_GL_GetDrawableSize( SDL_window, &config->vidWidth, &config->vidHeight );
+#endif
 
 	// save render dimensions as renderer may change it in advance
 	glw_state.window_width = config->vidWidth;
@@ -520,24 +411,20 @@ static int GLW_SetMode( int mode, const char *modeFS, qboolean fullscreen, qbool
 GLimp_StartDriverAndSetMode
 ===============
 */
-static rserr_t GLimp_StartDriverAndSetMode( int mode, const char *modeFS, qboolean fullscreen, qboolean vulkan )
-{
+static rserr_t GLimp_StartDriverAndSetMode( int mode, const char *modeFS, qboolean fullscreen, qboolean vulkan ) {
 	rserr_t err;
 
-	if ( fullscreen && in_nograb->integer )
-	{
+	if ( fullscreen && in_nograb->integer ) {
 		Com_Printf( "Fullscreen not allowed with \\in_nograb 1\n");
 		Cvar_Set( "r_fullscreen", "0" );
 		r_fullscreen->modified = qfalse;
 		fullscreen = qfalse;
 	}
 
-	if ( !SDL_WasInit( SDL_INIT_VIDEO ) )
-	{
+	if ( !SDL_WasInit( SDL_INIT_VIDEO ) ) {
 		const char *driverName;
 
-		if ( SDL_Init( SDL_INIT_VIDEO ) != 0 )
-		{
+		if ( SDL_Init( SDL_INIT_VIDEO ) != 0 ) {
 			Com_Printf( "SDL_Init( SDL_INIT_VIDEO ) FAILED (%s)\n", SDL_GetError() );
 			return RSERR_FATAL_ERROR;
 		}
@@ -547,10 +434,9 @@ static rserr_t GLimp_StartDriverAndSetMode( int mode, const char *modeFS, qboole
 		Com_Printf( "SDL using driver \"%s\"\n", driverName );
 	}
 
-	err = GLW_SetMode( mode, modeFS, fullscreen, vulkan );
+	err = GLW_SetMode( mode, modeFS, fullscreen );
 
-	switch ( err )
-	{
+	switch ( err ) {
 		case RSERR_INVALID_FULLSCREEN:
 			Com_Printf( "...WARNING: fullscreen unavailable in this mode\n" );
 			return err;
@@ -564,7 +450,6 @@ static rserr_t GLimp_StartDriverAndSetMode( int mode, const char *modeFS, qboole
 	return RSERR_OK;
 }
 
-
 /*
 ===============
 GLimp_Init
@@ -573,8 +458,7 @@ This routine is responsible for initializing the OS specific portions
 of OpenGL
 ===============
 */
-void GLimp_Init( glconfig_t *config )
-{
+void GLimp_Init( glconfig_t *config ) {
 	rserr_t err;
 
 #ifndef _WIN32
@@ -594,19 +478,15 @@ void GLimp_Init( glconfig_t *config )
 
 	// Create the window and set up the context
 	err = GLimp_StartDriverAndSetMode( r_mode->integer, r_modeFullscreen->string, r_fullscreen->integer, qfalse );
-	if ( err != RSERR_OK )
-	{
-		if ( err == RSERR_FATAL_ERROR )
-		{
+	if ( err != RSERR_OK ) {
+		if ( err == RSERR_FATAL_ERROR ) {
 			Com_Error( ERR_FATAL, "GLimp_Init() - could not load OpenGL subsystem" );
 			return;
 		}
 
-		if ( r_mode->integer != 3 || ( r_fullscreen->integer && atoi( r_modeFullscreen->string ) != 3 ) )
-		{
+		if ( r_mode->integer != 3 || ( r_fullscreen->integer && atoi( r_modeFullscreen->string ) != 3 ) ) {
 			Com_Printf( "Setting \\r_mode %d failed, falling back on \\r_mode %d\n", r_mode->integer, 3 );
-			if ( GLimp_StartDriverAndSetMode( 3, "", r_fullscreen->integer, qfalse ) != RSERR_OK )
-			{
+			if ( GLimp_StartDriverAndSetMode( 3, "", r_fullscreen->integer, qfalse ) != RSERR_OK ) {
 				// Nothing worked, give up
 				Com_Error( ERR_FATAL, "GLimp_Init() - could not load OpenGL subsystem" );
 				return;
@@ -622,7 +502,6 @@ void GLimp_Init( glconfig_t *config )
 	Key_ClearStates();
 }
 
-
 /*
 ===============
 GLimp_EndFrame
@@ -630,15 +509,12 @@ GLimp_EndFrame
 Responsible for doing a swapbuffers
 ===============
 */
-void GLimp_EndFrame( void )
-{
+void GLimp_EndFrame( void ) {
 	// don't flip if drawing to front buffer
-	if ( Q_stricmp( cl_drawBuffer->string, "GL_FRONT" ) != 0 )
-	{
+	if ( Q_stricmp( cl_drawBuffer->string, "GL_FRONT" ) != 0 ) {
 		SDL_GL_SwapWindow( SDL_window );
 	}
 }
-
 
 /*
 ===============
@@ -647,11 +523,9 @@ GL_GetProcAddress
 Used by opengl renderers to resolve all qgl* function pointers
 ===============
 */
-void *GL_GetProcAddress( const char *symbol )
-{
+void *GL_GetProcAddress( const char *symbol ) {
 	return SDL_GL_GetProcAddress( symbol );
 }
-
 
 #ifdef USE_VULKAN_API
 /*
@@ -662,8 +536,7 @@ This routine is responsible for initializing the OS specific portions
 of Vulkan
 ===============
 */
-void VKimp_Init( glconfig_t *config )
-{
+void VKimp_Init( glconfig_t *config ) {
 	rserr_t err;
 
 #ifndef _WIN32
@@ -713,39 +586,33 @@ void VKimp_Init( glconfig_t *config )
 	Key_ClearStates();
 }
 
-
 /*
 ===============
 VK_GetInstanceProcAddr
 ===============
 */
-void *VK_GetInstanceProcAddr( VkInstance instance, const char *name )
-{
+void *VK_GetInstanceProcAddr( VkInstance instance, const char *name ) {
 	return qvkGetInstanceProcAddr( instance, name );
 }
-
 
 /*
 ===============
 VK_CreateSurface
 ===============
 */
-qboolean VK_CreateSurface( VkInstance instance, VkSurfaceKHR *surface )
-{
+qboolean VK_CreateSurface( VkInstance instance, VkSurfaceKHR *surface ) {
 	if ( SDL_Vulkan_CreateSurface( SDL_window, instance, surface ) == SDL_TRUE )
 		return qtrue;
 	else
 		return qfalse;
 }
 
-
 /*
 ===============
 VKimp_Shutdown
 ===============
 */
-void VKimp_Shutdown( qboolean unloadDLL )
-{
+void VKimp_Shutdown( qboolean unloadDLL ) {
 	IN_Shutdown();
 
 	SDL_DestroyWindow( SDL_window );
@@ -759,7 +626,6 @@ void VKimp_Shutdown( qboolean unloadDLL )
 }
 #endif // USE_VULKAN_API
 
-
 /*
 ================
 GLW_HideFullscreenWindow
@@ -771,14 +637,12 @@ void GLW_HideFullscreenWindow( void ) {
 	}
 }
 
-
 /*
 ===============
 Sys_GetClipboardData
 ===============
 */
-char *Sys_GetClipboardData( void )
-{
+char *Sys_GetClipboardData( void ) {
 #ifdef DEDICATED
 	return NULL;
 #else
@@ -801,14 +665,12 @@ char *Sys_GetClipboardData( void )
 #endif
 }
 
-
 /*
 ===============
 Sys_SetClipboardBitmap
 ===============
 */
-void Sys_SetClipboardBitmap( const byte *bitmap, int length )
-{
+void Sys_SetClipboardBitmap( const byte *bitmap, int length ) {
 #ifdef _WIN32
 	HGLOBAL hMem;
 	byte *ptr;
