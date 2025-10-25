ab3c4cd066e89ec5ccbd06adf3aef6bd8bca92b5
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-10-26 09:34:48 +1200

    Update snd_mem.c

--- code/client/snd_mem.c
+++ code/client/snd_mem.c
@@ -32,7 +32,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "snd_local.h"
 #include "snd_codec.h"
 
+#if ARCH == x86
+    #define DEF_COMSOUNDMEGS 8
+#elif ARCH == x86_64
+    #define DEF_COMSOUNDMEGS 256
+#else
     #define DEF_COMSOUNDMEGS 8  // defaults
+#endif
 
 /*
 ===============================================================================
