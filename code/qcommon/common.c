b820d9e5aa754752a5ad0b92e1ff6e4f0b8dafd7
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-10-26 14:33:40 +1200

    Update common.c

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -35,11 +35,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../client/keys.h"
 
 #if ARCH == x86
-    #define DEF_COMHUNKMEGS 768
+    #define DEF_COMHUNKMEGS 1023
 #elif ARCH == x86_64
-    #define DEF_COMHUNKMEGS 1024
+    #define DEF_COMHUNKMEGS 2047
 #else
-    #define DEF_COMHUNKMEGS 1024  // defaults
+    #define DEF_COMHUNKMEGS 2047  // defaults
 #endif
 #define DEF_COMZONEMEGS			12
 
