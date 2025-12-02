17ac7e73049b098965f272a6e7e0cbb2e3c50d66
noire-dev <141606820+noire-dev@users.noreply.github.com>
2025-12-02 21:00:24 +1200

    Update: 2025-12-02 21:00 New memory system, done

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -1727,7 +1727,7 @@ void *Hunk_Alloc( int size, ha_pref preference ) {
     
     buf = (void *)(s_hunkData + s_hunkUsed);
     s_hunkUsed += size;
-    Com_Printf("Hunk_Alloc: allocating %ikb. (used=%dmb, total=%dmb)", size/1024, s_hunkUsed/1024/1024, s_hunkTotal/1024/1024)
+    Com_Printf("Hunk_Alloc: allocating %ikb. (used=%dmb, total=%dmb)", size/1024, s_hunkUsed/1024/1024, s_hunkTotal/1024/1024);
 
     Com_Memset( buf, 0, size );
 	return buf;
