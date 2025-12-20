812a5982605a442b54eed98cabb0a95a201e2f2f
noire-dev <141606820+noire-dev@users.noreply.github.com>
2025-12-20 18:38:53 +1200

    Update: 2025-12-20 18:38 clean

--- code/client/snd_dma.c
+++ code/client/snd_dma.c
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "snd_local.h"
 #include "snd_codec.h"
 #include "client.h"
-#include "../cm_public.h"
+#include "../qcommon/cm_public.h"
 
 static void S_Update_( int msec );
 static void S_UpdateBackgroundTrack( void );
